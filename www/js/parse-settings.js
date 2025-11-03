26af832c09733c3c92b56b5751a5c1b637bea7a3
Manuel Biagini <55558507+Brazzo978@users.noreply.github.com>
2025-11-03 11:06:04 +0100

    Harden APN configuration form

--- www/js/parse-settings.js
+++ www/js/parse-settings.js
@@ -1,112 +1,93 @@
 function parseCurrentSettings(rawdata) {
-  const data = rawdata;
-
-  const lines = data.split("\n");
+  const lines = rawdata.split("\n");
   console.log(lines);
 
-  // Remove QUIMSLOT and only take 1 or 2
-  this.sim = lines
-    .find(
-      (line) => line.includes("SIM1 ENABLE") || line.includes("SIM2 ENABLE")
-    )
-    .split(" ")[0]
-    // remove spaces
-    .replace(/\D/g, "");
-  // .replace(/\"/g, "");
-
-  try {
-    this.apn = lines
-      .find((line) => line.includes("+CGCONTRDP: 1"))
-      .split(",")[2]
-      .replace(/\"/g, "");
-  } catch (error) {
-    this.apn = "Failed fetching APN";
+  let sim = "-";
+  const simLine = lines.find(
+    (line) => line.includes("SIM1 ENABLE") || line.includes("SIM2 ENABLE")
+  );
+  if (simLine) {
+    sim = simLine.split(" ")[0].replace(/\D/g, "");
   }
 
-  this.cellLock4GStatus = lines
-    .find((line) => line.includes('LTE,Enable Bands :'))
-    .split(":")[1]
-    .replace(/\"/g, "");
+  let apn = "Failed fetching APN";
+  const apnLine = lines.find((line) => line.includes("+CGCONTRDP: 1"));
+  if (apnLine) {
+    apn = apnLine.split(",")[2].replace(/\"/g, "");
+  }
 
-  this.cellLock5GStatus = lines
-    .find((line) => line.includes('NR5G_SA,Enable Bands :'))
-    .split(":")[1]
-    .replace(/\"/g, "");
+  let apnIP = "-";
+  const apnIpLine = lines.find((line) => line.includes("+CGDCONT: 1"));
+  if (apnIpLine) {
+    apnIP = apnIpLine.split(",")[1].replace(/\"/g, "");
+  }
 
-  const prefNetworks = lines
-    .find((line) => line.includes('^SLMODE:'))
-    .split(":")[1]
-    .split(",")[1]
-    .replace(/\"/g, "").trim();
-  if( prefNetworks === "7"){
-	this.prefNetwork="AUTO"
-	}else if (prefNetworks === "2"){
-		this.prefNetwork="LTE Only"
-	}else if (prefNetworks === "6"){
-                this.prefNetwork="NR5G-NSA"
-        }else if (prefNetworks === "4"){
-                this.prefNetwork="NR5G-SA"
-        }
+  let cellLock4GStatus = "0";
+  const cellLock4GLine = lines.find((line) =>
+    line.includes("LTE,Enable Bands :")
+  );
+  if (cellLock4GLine) {
+    cellLock4GStatus = cellLock4GLine.split(":")[1].replace(/\"/g, "");
+  }
 
-/*
-  this.nrModeControlStatus = lines
-    .find((line) => line.includes('^SLMODE:'))
-    .split(":")[1]
-    .split(",")[1]
-    .replace(/\"/g, "").trim();
-*/
-  this.apnIP = lines
-    .find((line) => line.includes("+CGDCONT: 1"))
-    .split(",")[1]
-    .replace(/\"/g, "");
+  let cellLock5GStatus = "0";
+  const cellLock5GLine = lines.find((line) =>
+    line.includes("NR5G_SA,Enable Bands :")
+  );
+  if (cellLock5GLine) {
+    cellLock5GStatus = cellLock5GLine.split(":")[1].replace(/\"/g, "");
+  }
 
-  try {
-    const PCCbands = lines
-      .find((line) => line.includes('PCC info:'))
+  let prefNetwork = "-";
+  let prefNetworksValue = null;
+  const prefNetworkLine = lines.find((line) => line.includes("^SLMODE:"));
+  if (prefNetworkLine) {
+    prefNetworksValue = prefNetworkLine
       .split(":")[1]
-      .split("_")[1]
-      .replace(/\D/g, "");
-      //.replace(/\"/g, "");
-    
-    // Loop over all QCAINFO: "SCC" lines and get the bands
-    try {
-      const SCCbands = lines
-        .filter((line) => line.includes('SCC'))
-        .map((line) => line.split(":")[1].split("_")[1].replace(/\D/g, ""))
-        .join(", ");
-      this.bands = `${PCCbands}, ${SCCbands}`;
-    } catch (error) {
-      this.bands = PCCbands;
+      .split(",")[1]
+      .replace(/\"/g, "")
+      .trim();
+
+    if (prefNetworksValue === "7") {
+      prefNetwork = "AUTO";
+    } else if (prefNetworksValue === "2") {
+      prefNetwork = "LTE Only";
+    } else if (prefNetworksValue === "6") {
+      prefNetwork = "NR5G-NSA";
+    } else if (prefNetworksValue === "4") {
+      prefNetwork = "NR5G-SA";
     }
-    
-  } catch (error) {
-    this.bands = "Failed fetching bands";
   }
 
-  if (this.cellLock4GStatus == 1 && this.cellLock5GStatus == 1) {
-    this.cellLockStatus = "Locked to 4G and 5G";
-  } else if (this.cellLock4GStatus == 1) {
-    this.cellLockStatus = "Locked to 4G";
-  } else if (this.cellLock5GStatus == 1) {
-    this.cellLockStatus = "Locked to 5G";
-  } else {
-    this.cellLockStatus = "Not Locked";
+  let bands = "Failed fetching bands";
+  const pccLine = lines.find((line) => line.includes("PCC info:"));
+  if (pccLine) {
+    const pccBands = pccLine.split(":")[1].split("_")[1].replace(/\D/g, "");
+
+    const sccBands = lines
+      .filter((line) => line.includes("SCC"))
+      .map((line) => line.split(":")[1].split("_")[1].replace(/\D/g, ""))
+      .filter((value) => value.length > 0)
+      .join(", ");
+
+    bands = sccBands ? `${pccBands}, ${sccBands}` : pccBands;
   }
 
-  if (prefNetworks == "7") {
-    this.nrModeControlStatus = "Enable All";
-  } else if (prefNetworks == 6) {
-    this.nrModeControlStatus = "SA Disabled";
-  } else {
-    this.nrModeControlStatus = "NSA Disabled";
+  let cellLockStatus = "Not Locked";
+  if (cellLock4GStatus == 1 && cellLock5GStatus == 1) {
+    cellLockStatus = "Locked to 4G and 5G";
+  } else if (cellLock4GStatus == 1) {
+    cellLockStatus = "Locked to 4G";
+  } else if (cellLock5GStatus == 1) {
+    cellLockStatus = "Locked to 5G";
   }
+
   return {
-    sim: sim,
-    apn: apn,
-    apnIP: apnIP,
-    cellLockStatus: cellLockStatus,
-    prefNetwork: prefNetwork,
-    nrModeControl: nrModeControlStatus,
-    bands: bands,
+    sim,
+    apn,
+    apnIP,
+    cellLockStatus,
+    prefNetwork,
+    bands,
   };
 }
